25e6b3d2107e9bf431edcdafc11e0d9f8eb73624
Renato Rodrigues Dos Reis <renatorodrigues_reis@outlook.com>
2021-10-08 00:22:38 -0300

    Criando atividade aula online

---
+++ pecas.js
@@ -0,0 +1,26 @@
+let peso = 120; 
+
+    console.log(peso)
+
+if(peso  > 100) {
+    console.log("A peca possui o peso adequado, pode ser cadastrada!")
+} else {
+    console.log("A peca nao pode ser cadastrada, nao possui o peso minimo!")
+}
+
+let listaPecas = ["freio","motor de arranque","bobina","Disco de freio","fio"]
+    console.log (listaPecas)
+if (listaPecas.length < 5 ) {
+    
+    console.log("A lista possui espaco")
+} else {
+    console.log("Espaco maximo atingido")
+
+}
+    listaPecas.push("oleo diesel")
+
+    console.log(listaPecas)
+    
+
+
+ 
