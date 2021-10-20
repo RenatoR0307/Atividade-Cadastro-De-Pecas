0fba2a57ee7074f80d2835e25e104ec4b850c008
Renato Rodrigues Dos Reis <renatorodrigues_reis@outlook.com>
2021-10-19 22:26:08 -0300

    Atualizacao de lista de pecas

--- pecas.js
+++ pecas.js
@@ -1,47 +1,40 @@
 console.log("Digite o peso da peca")
-let peso = 120; 
+let peso = 120;
 
-    console.log(peso)
+console.log(peso)
 
-if(peso  > 100) 
-{
+if (peso > 100) {
     console.log("A peca possui o peso adequado, pode ser cadastrada!")
-    
-    let listaPecas = ["freio","motor de arranque","bobina","Disco de freio","fio"]
-if (listaPecas.length < 10 ) 
-{
-     console.log("A lista possui espaco, digite o nome da peca.")
 
- let peca = "Vela"
-    console.log (peca)
-    
-if (peca.length > 3) 
-{
+    let listaPecas = ["freio", "motor de arranque", "bobina", "Disco de freio", "fio"]
+    if (listaPecas.length < 6) {
+        console.log("A lista possui espaco, digite o nome da peca.")
 
-    listaPecas.push(peca)
+        let peca = "vela"
+        console.log(peca)
 
-    console.log("Nome da peca e valido pode seguir com cadastro!")
+        if (peca.length > 3) {
 
-for (let i = 0; i < listaPecas.length; i++) 
-{
-    const listaAtual = listaPecas[i];
-    console.log("Pecas: " + listaAtual)
-}
+            listaPecas.push(peca)
 
-}
-else 
-{
-    console.log("Nome da peca menor que o permitido ")
-}  
+            console.log("Nome da peca e valido pode seguir com cadastro!")
 
-}
-else
-{
-    console.log("Espaco maximo atingido")
-}
+            for (let i = 0; i < listaPecas.length; i++) {
+                const listaAtual = listaPecas[i];
+                console.log("Peca: " + listaAtual)
+            }
+
+        }
+        else {
+            console.log("Nome da peca menor que o permitido ")
+        }
+
+    }
+    else {
+        console.log("Espaco maximo atingido")
+    }
 
 }
-else
-{
+else {
     console.log("A peca nao pode ser cadastrada, nao possui o peso minimo!")
 }
